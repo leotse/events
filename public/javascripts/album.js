123de4d32b9b8b67370a314ad733af3d7952ac4a
kso <kent.so@digiflare.com>
2013-10-19 00:06:11 -0400

    Added swipe for iPad
    Also synced the index of the slide show with the next / prev
    Changed colour scheme to match Alan's request

--- public/javascripts/album.js
+++ public/javascripts/album.js
@@ -1,5 +1,7 @@
 function initAlbum(eventId, pollInterval, slideInterval, debugMode) {
 
+  var IS_IPAD = navigator.userAgent.match(/iPad/i) != null;
+
   var maxId = "", minId = "";
   var cachedList = []; // cache of all the images loaded in view
   var newImages = []; // cache of all the new images in polling, but not loaded
@@ -17,6 +19,9 @@ function initAlbum(eventId, pollInterval, slideInterval, debugMode) {
   // ensures no double lazy loading
   var loading = false;
 
+  // swipe-able carousel -- for iPad only
+  var carousel;
+
   // refresh UI with the new data
   function refresh(data, prepend) {
     var $medialist = $(".media");
@@ -26,7 +31,7 @@ function initAlbum(eventId, pollInterval, slideInterval, debugMode) {
     if(length > 0) {
       for (var i = 0; i < length; i++) {
         // create image
-        var $img = $("<img class='fade2' src='" + data[i].images.thumbnail.url + "' rel='" + data[i].id + "'/>");
+        var $img = $("<img class='fade23' src='" + data[i].images.thumbnail.url + "' rel='" + data[i].id + "'/>");
         var $figure = $("<figure></figure>");
 
         // append image
@@ -86,32 +91,46 @@ function initAlbum(eventId, pollInterval, slideInterval, debugMode) {
     $(".overlay").addClass("active slideshow");
     $("body").attr("style", "overflow: hidden");
     if(slideTimer !== null) { clearTimeout(slideTimer); }
+    if(IS_IPAD) {
+      $("#media_swipe_wrapper").html(""); // clear html
+      createSwipe();      
+    }
     slideNext();
   }
 
   function slideNext() {
-    // increment index or reset current index if already reached the end
-    if(++slideCurrentIndex === cachedList.length) {   
-      slideCurrentIndex = 0;
+    if(IS_IPAD) {
+      carousel.next();
     }
-    if(debugMode) console.log(slideCurrentIndex);
-    var media = cachedList[slideCurrentIndex];
-    var created = moment.unix(media.created_time);
-    var fx = fxlist[slideCurrentIndex % fxlist.length];
-    // var fxout = fxoutlist[slideCurrentIndex % fxoutlist.length];
-    $(".media_details").html("");
-    $(".media_details img").show();
-    $(".media_details").append("<div class='details_desc fade1'>"+media.caption.text+"</div>");
-    $(".media_details").append("<img class='details_author_img fade2' src='"+media.user.profile_picture+"' />");
-    $(".media_details").append("<div class='details_author fade2'>By: "+media.user.full_name+"</div>");
-    $(".media_details").append("<div class='details_created fade3'>Date: "+(created.isBefore(new Date(), "day")? created.format("YYYY-MM-DD"): created.fromNow())+"</div>");
-    $(".media_img_container img").attr("src", media.images.standard_resolution.url);
-    $(".media_img_container img").removeClass(); // clear class
-    $(".media_img_container img").addClass('animated '+fx); // add animation class
-    
-    $(".media_details img").error(function () { 
-        $(this).hide();
-    });
+    else {
+      // increment index or reset current index if already reached the end    
+      if(++slideCurrentIndex === cachedList.length) {   
+        slideCurrentIndex = 0;
+      }
+      mediaIndex = slideCurrentIndex;
+      if(debugMode) console.log(slideCurrentIndex);
+      var media = cachedList[slideCurrentIndex];
+      var created = moment.unix(media.created_time);
+      var fx = fxlist[slideCurrentIndex % fxlist.length];
+      // var fxout = fxoutlist[slideCurrentIndex % fxoutlist.length];
+      $(".media_details").html("");
+      $(".media_details img").show();
+      $(".media_details").append("<div class='details_desc fade1'>"+media.caption.text+"</div>");
+      $(".media_details").append("<img class='details_author_img fade2' src='"+media.user.profile_picture+"' />");
+      $(".media_details").append("<div class='details_author fade2'>By: "+media.user.full_name+"</div>");
+      $(".media_details").append("<div class='details_created fade3'>Date: "+(created.isBefore(new Date(), "day")? created.format("YYYY-MM-DD"): created.fromNow())+"</div>");
+      $(".media_img_container img").attr("src", media.images.standard_resolution.url);
+      $(".media_img_container img").removeClass(); // clear class
+      $(".media_img_container img").addClass('animated '+fx); // add animation class
+      
+      $(".media_details img").error(function () { 
+          $(this).attr("src", "/images/icons/anonymousUser.jpg");
+      });
+      $(".media_img_container img").error(function () { 
+          $(this).attr("src", "/images/logo4.png");
+      });
+    }
+
     // recur slideNext    
     slideTimer = setTimeout(slideNext, slideInterval); // to-do: set this to 15 min for production
   }
@@ -147,10 +166,22 @@ function initAlbum(eventId, pollInterval, slideInterval, debugMode) {
       $(".media_details").append("<img class='details_author_img fade2' src='"+media.user.profile_picture+"' />");
       $(".media_details").append("<div class='details_author fade2'>By: "+media.user.full_name+"</div>");
       $(".media_details").append("<div class='details_created fade3'>Date: "+(created.isBefore(new Date(), "day")? created.format("YYYY-MM-DD"): created.fromNow())+"</div>");
-      $(".media_img_container img").attr("src", media.images.standard_resolution.url);
-      $(".media_img_container img").attr("class", "fade1");
+
+      // ipad hacks for Alan's wedding; to be cleaned up
+      if(IS_IPAD) {
+        $("div.action_next_prev_img").hide();     
+      }
+      else {
+        $("div.action_next_prev_img").show();
+        $(".media_img_container img").attr("src", media.images.standard_resolution.url);
+        $(".media_img_container img").attr("class", "fade1");
+      }
+
       $(".media_details img").error(function () { 
-          $(this).hide();
+          $(this).attr("src", "/images/icons/anonymousUser.jpg");
+      });
+      $(".media_img_container img").error(function () { 
+          $(this).attr("src", "/images/logo4.png");
       });
     }
   }
@@ -178,8 +209,66 @@ function initAlbum(eventId, pollInterval, slideInterval, debugMode) {
 
     if(id !== "") {
       mediaIndex = findMediaItemById(id);
-      loadMediaDetails(mediaIndex);
+
+      if(IS_IPAD) {
+        $("#media_swipe_wrapper").html(""); // clear html
+        createSwipe();
+        carousel.goToPage(mediaIndex);
+      }
+      else {
+        loadMediaDetails(mediaIndex);
+      }
     }
+  }
+
+  function populateDetails(media) {
+    var created = moment.unix(media.created_time);
+    return '<div class="media_popup"><div class="media_img_container"><img src="'+ media.images.standard_resolution.url +'" class="fade1"></div><div class="overlay_close"></div><div class="media_details"><div class="details_desc fade1">'+media.caption.text+'</div><img class="details_author_img fade2" src="'+media.user.profile_picture+'" ><div class="details_author fade2">By: '+media.user.full_name+'</div><div class="details_created fade3">Date: '+(created.isBefore(new Date(), "day")? created.format("YYYY-MM-DD"): created.fromNow())+'</div></div></div>';
+  }
+
+  function createSwipe() {
+    var el,
+        i,
+        page,
+        slides = [];
+
+    // load slides
+    for(i=0; i<cachedList.length; i++) {
+      slides.push(populateDetails(cachedList[i]));
+    }
+
+    carousel = new SwipeView('#media_swipe_wrapper', {
+      numberOfPages: slides.length,
+      hastyPageFlip: true
+    });
+
+    // Load initial data
+    for (i=0; i<3; i++) {
+      page = i==0 ? slides.length-1 : i-1;
+
+      el = document.createElement('span');
+      el.innerHTML = slides[page];
+      carousel.masterPages[i].appendChild(el)
+    }
+
+    carousel.onFlip(function () {
+      var el,
+        upcoming,
+        i;
+
+      for (i=0; i<3; i++) {
+        upcoming = carousel.masterPages[i].dataset.upcomingPageIndex;
+
+        if (upcoming != carousel.masterPages[i].dataset.pageIndex) {
+          el = carousel.masterPages[i].querySelector('span');
+          el.innerHTML = slides[upcoming];
+
+          $(".media_details img").error(function () { 
+              $(this).hide();
+          });
+        }
+      }
+    });
   }
 
   function scrollHandler() {
@@ -196,13 +285,24 @@ function initAlbum(eventId, pollInterval, slideInterval, debugMode) {
     }    
   }
 
-  $(document).ready(function() {
+
+  $(document).ready(function() { 
     myHeader = $('header');
     myContentHeader = $('.content_header');
     myHeader.data( 'position', myHeader.position() );
     myContentHeader.data( 'position', myContentHeader.position() );
 
-    $(window).scroll(scrollHandler);
+    if(IS_IPAD) {
+      $(window).bind("touchmove touchend touchstart scroll", scrollHandler);  
+      // append placeholders DOM
+      var i;
+      for(i=0; i<1000; i++) {
+        $("#swipe-wrap").append("<div id='img_"+i+"'></div>");
+      }
+    }
+    else {
+      $(window).scroll(scrollHandler);  
+    }  
 
     $('.end_of_media').bind('inview', function(event, visible) {
       if (visible) {
@@ -214,5 +314,7 @@ function initAlbum(eventId, pollInterval, slideInterval, debugMode) {
     $('#action_slideshow').click(slideshowHandler);
     $('.action_next_prev_img a').click(nextPrevImgHandler);
     $(".overlay").click(clickToClose);
+
+
   });
 }
